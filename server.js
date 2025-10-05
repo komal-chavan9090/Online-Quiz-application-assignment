6c67b4d42f35ecfde0f4e782ca1d64c49e1c4dd1
komal-chavan9090 <155181393+komal-chavan9090@users.noreply.github.com>
2025-10-05 13:56:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const app = require('./src/app');
 require('dotenv').config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-  console.log(`Health check: http://localhost:${PORT}/api/health`);
 });
+
